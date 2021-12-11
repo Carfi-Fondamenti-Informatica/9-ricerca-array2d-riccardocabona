39072bd18dd066fd1c83bd163aab2ee42807d2f9
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-12-11 15:04:51 +0000

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include"lib.h"
+#include <cstring>
+bool cercanome(char nome[1][20],char lista[10][20],int &pos) {
+    for (int i = 0; i < 10; i++) {
+        if (strcmp(nome[0],lista[i])==0) {
+            pos=i;
+            return true;
+        }
+    }
+    return false;
+}
