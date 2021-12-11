dce553df938dbb9d99bc8ec1bfa0b86cd8bdf4b6
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-12-11 15:04:05 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include"lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lista[10][20];
+    char nome[1][20];
+    int pos;
+    for(int i=0;i<10;i++){
+        cin>>lista[i];
+    }
+    cin>>nome[0];
+    if(cercanome(nome,lista,pos)){
+        cout<<pos;
+    }else{
+        cout<<"non presente";
+    }
+
+    return 0;
 }
